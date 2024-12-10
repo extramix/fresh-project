5a0d8f432e7223e0e630ad7b0052c04b78e7c6a6
Mix Sirapat <sirapat.jr@gmail.com>
2024-12-10 16:22:24 +0900

    add countdown page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,9 +6,11 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $about from "./routes/about.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
+import * as $countdown from "./routes/countdown.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $search from "./routes/search.tsx";
+import * as $Countdown from "./islands/Countdown.tsx";
 import * as $Counter from "./islands/Counter.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
@@ -18,11 +20,13 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/about.tsx": $about,
     "./routes/api/joke.ts": $api_joke,
+    "./routes/countdown.tsx": $countdown,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/search.tsx": $search,
   },
   islands: {
+    "./islands/Countdown.tsx": $Countdown,
     "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
